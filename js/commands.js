f6eeafcec0811d6202180274cb7a7464c52e889f
kartikadak <63922298+kartikadak@users.noreply.github.com>
2024-09-29 02:57:46 +0100

    Create commands.js

---
+++ js/commands.js
@@ -0,0 +1,81 @@
+var password = "Qwerty123";
+var linkedin = "https://www.linkedin.com/in/kartik-adak-81a25918a/";
+var github = "https://github.com/kartikadak/";
+var email = 'mailto:kartikadak98@gmail.com';
+var medium = "https://medium.com/@kartikadak98"
+
+whois = [
+  "<br>",
+  "Hey, I'm Kartik!",
+  "I am a security professional with 3+ years of experience in information security and incident response", 
+  "I have developed a strong foundation in detecting, responding to, and mitigating cyber security threats.",
+  "My expertise in digital forensics, malware analysis, and security monitoring, combined with hands-on experience in red teaming",
+  "and penetration testing, allows me to effectively secure organizations against advanced attackers.",
+  "Currently pursuing a Master’s in Cyber security Management, I am passionate about expanding my skills to protect customers",
+   "in real-time from evolving cyber threats.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of Who am I? is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/kartikadak' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/kartikadak' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  'Redirecting you to my Github Repository <a href="' + github + '" target="_blank">github/kartikadak' + "</a>",
+  "<br>"
+];
+
+blogs = [
+  "<br>",
+  'Redirecting you to my medium page <a href="' + medium + '" target="_blank">medium/kartikadak' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Kartik?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View security projects',
+  '<span class="command">blogs</span>          View cybersecurity blogs',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Kartik Adak (Kit3ak) Not A Corporation. All rights reserved.</span>',
+    "██ ▄█▀▄▄▄       ██▀███  ▄▄▄█████▓ ██▓ ██ ▄█▀    ▄▄▄      ▓█████▄  ▄▄▄       ██ ▄█▀",
+    "██▄█▒▒████▄    ▓██ ▒ ██▒▓  ██▒ ▓▒▓██▒ ██▄█▒    ▒████▄    ▒██▀ ██▌▒████▄     ██▄█▒ ",
+    "▓███▄░▒██  ▀█▄  ▓██ ░▄█ ▒▒ ▓██░ ▒░▒██▒▓███▄░    ▒██  ▀█▄  ░██   █▌▒██  ▀█▄  ▓███▄░ ",
+    "▓██ █▄░██▄▄▄▄██ ▒██▀▀█▄  ░ ▓██▓ ░ ░██░▓██ █▄    ░██▄▄▄▄██ ░▓█▄   ▌░██▄▄▄▄██ ▓██ █▄ ",
+    "▒██▒ █▄▓█   ▓██▒░██▓ ▒██▒  ▒██▒ ░ ░██░▒██▒ █▄    ▓█   ▓██▒░▒████▓  ▓█   ▓██▒▒██▒ █▄",
+    "▒ ▒▒ ▓▒▒▒   ▓▒█░░ ▒▓ ░▒▓░  ▒ ░░   ░▓  ▒ ▒▒ ▓▒    ▒▒   ▓▒█░ ▒▒▓  ▒  ▒▒   ▓▒█░▒ ▒▒ ▓▒",
+    "░ ░▒ ▒░ ▒   ▒▒ ░  ░▒ ░ ▒░    ░     ▒ ░░ ░▒ ▒░     ▒   ▒▒ ░ ░ ▒  ▒   ▒   ▒▒ ░░ ░▒ ▒░",
+    "░ ░░ ░  ░   ▒     ░░   ░   ░       ▒ ░░ ░░ ░      ░   ▒    ░ ░  ░   ░   ▒   ░ ░░ ░ ",
+    "░  ░        ░  ░   ░               ░  ░  ░            ░  ░   ░          ░  ░░  ░   ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+
